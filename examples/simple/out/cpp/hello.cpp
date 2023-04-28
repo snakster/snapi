a35f8e12ca526f456ca97be0e7d64acab6cf8381
Sebastian <jeckel.sebastian@gmail.com>
2023-04-28 11:55:54 +0200

    Include generated example output

---
+++ examples/simple/out/cpp/hello.cpp
@@ -0,0 +1,20 @@
+#include "hello.h"
+
+#include <iostream>
+
+using namespace std;
+
+void hello()
+{
+    cout << "I will stop writing boilerplate code." << endl;
+    cout << "I will stop writing boilerplate code." << endl;
+    cout << "I will stop writing boilerplate code." << endl;
+    cout << "I will stop writing boilerplate code." << endl;
+    cout << "I will stop writing boilerplate code." << endl;
+    cout << "I will stop writing boilerplate code." << endl;
+    cout << "I will stop writing boilerplate code." << endl;
+    cout << "I will stop writing boilerplate code." << endl;
+    cout << "I will stop writing boilerplate code." << endl;
+    cout << "I will stop writing boilerplate code." << endl;
+    cout << "I will stop writing boilerplate code." << endl;
+}
