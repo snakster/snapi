e3bb061db602766ee4b6a5b944f9fb395eb2dc9f
Sebastian <jeckel.sebastian@gmail.com>
2023-04-28 15:18:22 +0200

    Refactor into multiple files

--- pkg/snapi/generator.py
+++ pkg/snapi/generator.py
@@ -1,14 +1,12 @@
-import yaml
-import os
-from argparse import ArgumentParser
-
 from typing import Any, Dict
 from collections.abc import Callable
 
-from jinja2 import Environment, FunctionLoader, select_autoescape, TemplateNotFound, nodes
-from jinja2.ext import Extension
+import os
+import yaml
 
 from os.path import join, exists, getmtime
+
+from . import template
 
 
 class Generator:
@@ -18,17 +16,7 @@ class Generator:
         self._transformer_decls = {}
         self._output_decls = {}
 
-        self._template_env = Environment(
-            loader=FunctionLoader(load_template),
-            extensions=[SectionExtension],
-            autoescape=select_autoescape(),
-            trim_blocks=True,
-            lstrip_blocks=True
-        )
-
-        if section_delim == None:
-            section_delim = default_section_delim
-        self._template_env.section_delim_selector=section_delim
+        self._template_env = template.make_new_env(section_delim)
 
 
     def add_inputs(self, name: str, impl: Callable[..., None], args = {}):
@@ -134,105 +122,3 @@ def write_output_file(env, template_path: str, output_path: str, data: Any):
 
     with open(output_path, 'w') as f:
         f.write(s)
-
-
-
-class SectionExtension(Extension):
-    tags = {"section"}
-
-    def __init__(self, environment):
-        super().__init__(environment)
-
-        environment.extend(
-            section_data=None,
-            section_output_path="",
-            section_delim_selector=None
-        )
-
-    def parse(self, parser):
-        lineno = next(parser.stream).lineno
-        section_name = parser.parse_expression()
-        body = parser.parse_statements(["name:endsection"], drop_needle=True)
-
-        return nodes.CallBlock(
-            self.call_method("_section_lookup", [section_name]), [], [], body
-        ).set_lineno(lineno)
-
-    def _section_lookup(self, name, caller):
-        output_path = self.environment.section_output_path 
-        delim = self.environment.section_delim_selector(output_path)
-
-        if self.environment.section_data == None:
-            self.environment.section_data = load_section_data(output_path, delim)
-        
-        rv = self.environment.section_data.get(name)
-        if rv == None:
-            rv = caller()
-
-        indent = get_indent(rv)
-        marker = delim + name
-
-        return indent + marker + '\n' + rv + indent + marker
-
-
-def default_section_delim(fn: str) -> str:
-    if fn.endswith(".py"):
-        return "#$section:"
-    else:
-        return "//$section:"
-
-
-def load_template(path):
-    if not exists(path):
-        raise TemplateNotFound(path)
-
-    mtime = getmtime(path)
-    with open(path) as f:
-        source = f.read()
-
-    return source, path, lambda: mtime == getmtime(path)
-
-
-def load_section_data(path: str, delim: str) -> Dict[str,str]:
-    data = {}
-
-    try:
-        with open(path, 'r') as f:
-            while True:
-                s = f.readline()
-                if s == "":
-                    break
-                
-                delim_idx = s.find(delim)
-                if delim_idx == -1:
-                    continue
-
-                section_name = s[delim_idx+len(delim):].strip()
-
-                buf = ""
-
-                while True:
-                    s = f.readline()
-                    if s == "":
-                        break
-
-                    delim_idx = s.find(delim)
-                    if delim_idx == -1:
-                        buf += s
-                        continue
-
-                    if section_name != s[delim_idx+len(delim):].strip():
-                        break
-
-                    data[section_name] = buf
-    except IOError:
-        pass
-    
-    return data
-
-
-def get_indent(s: str):
-    for i in range(0, len(s)):
-        if not s[i].isspace():
-            return s[0:i]
-    return ""
